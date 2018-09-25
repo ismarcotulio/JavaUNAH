b61876d0f220f288b704a3b36ef9d1615fd9dd4f
martulioruiz <martulioruiz@gmail.com>
2018-09-25 07:53:44 -0600

    primer dia con git

---
+++ dia1/Alumn0o.java
@@ -0,0 +1,14 @@
+public class AlumnOo{
+	//definiendo propiedades del Alumno
+	public int NumeroCuenta;
+	public String NombreApellido;
+	public String CentroUniversitario;
+	public String Carrera;
+	public float PromedioActual;
+
+	//Sobreescribiendo el metodo toString()
+	public String toString(){
+		return "Numero Cuenta --> "+NumeroCuenta +"ln Nombre y apellido -->"+NombreApellido+"ln Centro Universitario"+CentroUniversitario+"ln Promedio-->"+PromedioActual
+	}
+
+}
---
+++ dia1/EjemploAlumno.java
@@ -0,0 +1,7 @@
+public class EjemploAlumnOo{
+	public static void main(String []args){
+		//Creando o instanciando un nuevo objeto
+		AlumnOo alumnoNumero1 = new AlumnOo();
+	}
+
+}
