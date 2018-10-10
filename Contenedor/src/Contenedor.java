84aa3ad41c39706176fd5e14adb2f3968e6f9b84
martulioruiz <martulioruiz@gmail.com>
2018-10-10 08:05:24 -0600

    se agrego proyecto netbeans

---
+++ Contenedor/src/Contenedor.java
@@ -0,0 +1,33 @@
+import java.util.Vector;
+public class Contenedor{
+
+	//Definiendo contenedor de clase Alumno
+	public Alumno AlumnoContenedor[] = new Alumno[100];
+	//metodos de la clase contenedor 
+	public void AgregarAlumno(Alumno alumno,int posicion){
+		if(AlumnoContenedor[posicion]==null){
+			AlumnoContenedor[posicion]=alumno;
+		}
+	};
+	public void EliminarAlumno(int posicion){
+		AlumnoContenedor[posicion]=null;
+	};
+        
+        public void EliminarAlumno(String palabra){
+            for(int x=0;x<AlumnoContenedor.length;x++){
+                if(AlumnoContenedor[x].Nombre==palabra){
+                    AlumnoContenedor[x]=null;
+                }
+            }
+        }; 
+        
+	public void EnlistarAlumno(){
+		System.out.println("Numero\t\tNombre\t\tCuenta\t\t");
+		for(int x=0;x<AlumnoContenedor.length;x++){
+			if(AlumnoContenedor[x]!=null){	
+			System.out.println(x+"\t\t"+AlumnoContenedor[x].Nombre+"\t\t"+AlumnoContenedor[x].Cuenta);
+			}
+		}
+	};
+
+}
---
+++ Contenedor/src/TestContenedor.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+public class TestContenedor{
+	public static void main(String[]args){
+		int ciclo=1;
+		int opcion;
+		int posicion;
+                int desicion;
+		int error1=1;
+                String palabra;
+		
+		Contenedor miContenedor = new Contenedor();
+		Scanner leer = new Scanner(System.in);
+		while(ciclo==1){
+			System.out.println("\n----------------------------------");
+			System.out.println("Menu mantenimiento de Alumnos");
+			System.out.println("1-Ingresar un Alumno");
+			System.out.println("2-Eliminar un Alumno");
+			System.out.println("3-Enlistar Alumnos");
+			System.out.println("0-Salir\n");
+			System.out.println("----------------------------------");
+			System.out.print("\nIngrese una opcion: ");
+			opcion = leer.nextInt();
+			leer.reset();
+			switch(opcion){
+				case 1:
+					System.out.println("\n--------------------------------");
+					System.out.println("Eligio Ingresar Alumno..");
+					System.out.println("--------------------------------");
+					Alumno miAlumno = new Alumno();
+					while(error1==1){
+						System.out.print("Ingrese una posicion entre 1-100: ");
+						posicion = leer.nextInt();
+						leer.reset();
+						
+						if(posicion<=100 && posicion>0){
+							System.out.print("Ingrese el Nombre del Alumno: ");
+							miAlumno.Nombre = leer.next();
+							leer.reset();
+							System.out.print("Ingrese el numero de cuenta: ");
+							miAlumno.Cuenta = leer.nextInt();
+							leer.reset();
+							miContenedor.AgregarAlumno(miAlumno,posicion);
+							error1=0;
+							System.out.println("Alumno agregado exitosamente!\n");
+						}else{
+							System.out.println("error!");
+							System.out.println("Posicion no disponible...");
+							System.out.println("intentelo de nuevo");
+						}
+					}
+					error1=1;
+				break;
+				case 2:
+					System.out.println("\n------------------------------");
+					System.out.println("Eligio eliminar Alumno...");
+					System.out.println("------------------------------");
+                                        System.out.println("Como desea Eliminar?");
+                                                System.out.println("1-Nombre");
+                                                System.out.println("2-Id");
+                                                desicion = leer.nextInt();
+                                                leer.reset();
+                                                if(desicion==2){
+                                                    while(error1==1){
+                                                
+                                                        System.out.print("Ingrese la posicion del Alumno entre 1-100: ");
+                                                        posicion = leer.nextInt();
+                                                        leer.reset();
+
+                                                        if(posicion<=100 && posicion>0){
+                                                                miContenedor.EliminarAlumno(posicion);
+                                                                System.out.println("Alumno eliminado exitosamente!");
+                                                                error1=0;
+                                                        }else{
+                                                                System.out.println("error!");
+                                                                System.out.println("Posicion no disponible...");
+                                                                System.out.println("intentelo de nuevo");
+                                                        }
+                                                    }
+                                                    error1=1;
+                                                }else{
+                                                    while(error1==1){
+                                                        System.out.print("Ingrese El nombre del Alumno: ");
+                                                        palabra = leer.nextLine();
+                                                        leer.reset();
+                                                        miContenedor.EliminarAlumno(palabra);
+                                                    }
+                                                    
+                                                }
+					
+				break;
+				case 3:
+					System.out.println("\n-----------------------------");
+					System.out.println("Eligio enlistar Alumnos");
+					System.out.println("-----------------------------");
+					miContenedor.EnlistarAlumno();
+				break;	
+				case 0:
+					System.out.println("\n-----------------");	
+					System.out.println("Eligio Salir");
+					System.out.println("-----------------");
+					System.out.println("Gracias por utilizar nuestro servicio");
+					ciclo=0;
+				break;
+				default:
+					System.out.println("Error!");
+					System.out.println("Esa opcion no esta en la lista...");
+					System.out.println("Intentelo de nuevo\n");
+			}
+		}
+	}
+}
