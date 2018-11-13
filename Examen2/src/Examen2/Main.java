db6faf0e844f1987e7ca24abebad50cbee9e66df
martulioruiz <martulioruiz@gmail.com>
2018-11-13 16:23:10 -0600

    Solucion de errores del menu, primeros 2 metodos terminados

--- Examen2/src/Examen2/Main.java
+++ Examen2/src/Examen2/Main.java
@@ -8,6 +8,7 @@ public class Main {
        
                 int ciclo=1;
                 int identificador;
+                float area;
                 String nombre;
 		String opcion;
 		int posicion;
@@ -21,15 +22,14 @@ public class Main {
                 ContenedorCiudad miContenedorCiudad = new ContenedorCiudad();
 		Scanner leer = new Scanner(System.in);
 		while(ciclo==1){
-			System.out.println("\n----------------------------------");
+			System.out.println("\n------------------------------------------------------------------------------------------------------------------------");
 			System.out.println("Menu");
-                        System.out.print("1-Paises \t\t 2-Ciudades \t\t 3-Vinculacion \t\t 4-Listado \t\t 5-salir");
-                        System.out.print("\nIngrese una opcion: ");
-                        System.out.print("1.1-Agregar un Pais \t\t 2.1-Agregar Ciudad \t\t 3.1-Agregar una vinculacion\t\t 4.1-Listado del Pais \t\t ");
-			System.out.println("1.2- Eliminar una Pais \t\t 2.1-Eliminar una ciudad 3.2-Eliminar una vinculacion \t\t 4.2-Listado de las Ciudades");
-			System.out.println("1.3-Modificar paises \t\t 2.3-Modificar ciudades \t\t 3.3-Modificar Vinculacion");
-                        System.out.println("1.4-Listado del Pais \t\t 2.4-Listado de Ciudades");
-			System.out.println("----------------------------------");
+                        System.out.println("1-Paises \t\t 2-Ciudades \t\t 3-Vinculacion \t\t\t 4-Listado \t\t\t 5-salir");
+                        System.out.println("1.1-Agregar un Pais \t 2.1-Agregar Ciudad \t 3.1-Agregar una vinculacion\t 4.1-Listado del Pais \t\t ");
+			System.out.println("1.2- Eliminar una Pais \t 2.1-Eliminar una ciudad 3.2-Eliminar una vinculacion \t 4.2-Listado de las Ciudades");
+			System.out.println("1.3-Modificar paises \t 2.3-Modificar ciudades  3.3-Modificar Vinculacion");
+                        System.out.print("1.4-Listado del Pais \t 2.4-Listado de Ciudades");
+			System.out.println("\n------------------------------------------------------------------------------------------------------------------------");
 			System.out.print("\nIngrese una opcion: ");
 			opcion = leer.next();
 			leer.reset();
@@ -50,20 +50,22 @@ public class Main {
 						if(posicion<=100 && posicion>0){
                                                     error1 = miContenedorPais.ComprobarIdentificadorPais(posicion);
                                                         if(error1==0){
-                                                            System.out.println("Ingrese Identificador del Pais");
-                                                        identificador = leer.nextInt();
-                                                        miPais.setIdentificadorPais(identificador);
-                                                        leer.reset();
+                                                            
+                                                        miPais.setIdentificadorPais(posicion);
                                                         
 							System.out.print("Agregar el nombre del pais: ");
 							nombre = leer.next();
                                                         miPais.setNombrePais(nombre);
 							leer.reset();
-							/*System.out.print("");
-							miPais.Cuenta = leer.nextInt();
+                                                        System.out.print("Agregar el continente del pais: ");
+							nombre = leer.next();
+                                                        miPais.setContinente(nombre);
 							leer.reset();
-							miContenedor.AgregarAlumno(miAlumno,posicion);
-							error1=0;*/
+                                                        System.out.print("Agregar Area Territorial del pais: ");
+							area = leer.nextFloat();
+                                                        miPais.setAreaPais(area);
+							leer.reset();
+							miContenedorPais.AgregarPais(miPais, posicion);
 							System.out.println("Pais agregado exitosamente!\n");
                                                         }else{
                                                             System.out.println("Esa posicion ya esta ocupada...");
@@ -83,13 +85,7 @@ public class Main {
 					System.out.println("\n------------------------------");
 					System.out.println("Eligio eliminar un pais...");
 					System.out.println("------------------------------");
-                                        System.out.println("Como desea Eliminar?");
-                                                System.out.println("1-Nombre");
-                                                System.out.println("2-identificacion");
-                                                //System.out.println("3-Posicion");
-                                                desicion = leer.nextInt();
-                                                leer.reset();
-                                                if(desicion==2){
+                                        
                                                     while(error1==1){
                                                 
                                                         System.out.print("Ingrese la posicion del Pais entre 1-100: ");
@@ -97,9 +93,18 @@ public class Main {
                                                         leer.reset();
 
                                                         if(posicion<=100 && posicion>0){
+                                                            error1 = miContenedorPais.ComprobarIdentificadorPais(posicion);
+                                                            if(error1==1){
                                                                 miContenedorPais.EliminarPais(posicion);
                                                                 System.out.println("Pais eliminado exitosamente!");
                                                                 error1=0;
+                                                            }else{
+                                                                System.out.println("Codigo invalido...");
+                                                                System.out.println("Pais no existe...");
+                                                                System.out.println("intentelo de nuevo...");
+                                                                error1=1;
+                                                            }
+                                                                
                                                         }else{
                                                                 System.out.println("error!");
                                                                 System.out.println("Posicion no disponible...");
@@ -107,15 +112,7 @@ public class Main {
                                                         }
                                                     }
                                                     error1=1;
-                                                }else{
-                                                    while(error1==1){
-                                                        System.out.print("Ingrese El nombre del País: ");
-                                                        palabra = leer.nextLine();
-                                                        leer.reset();
-                                                        miContenedorPais.EliminarPais(palabra);
-                                                    }
-                                                    
-                                                }
+                                                
 					
 				break;
 				case "1.3":
@@ -323,5 +320,5 @@ public class Main {
 		}
 	}
 }
-    }
-}
+    
+
